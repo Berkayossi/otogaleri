81a4b15f73488687078494df937df5f12c20838a
Berkayossi <96797457+Berkayossi@users.noreply.github.com>
2026-01-15 23:16:59 +0300

    Delete galeri_web/oto_galeri directory

--- galeri_web/oto_galeri/galeri/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import anasayfa, arac_detay, araclar_listesi
-
-urlpatterns = [
-    path('', anasayfa, name='anasayfa'),
-    path('araclar/', araclar_listesi, name='araclar_listesi'),
-    path('arac/<int:arac_id>/', arac_detay, name='arac_detay'),
-]
